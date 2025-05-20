306ae23b62d67fbb2401b58f29dc0ef356052815
b-lov <max@bilov.de>
2025-05-20 09:34:49 +0200

    allowlist

--- win/ps/useful_ps_commands.ps1
+++ win/ps/useful_ps_commands.ps1
@@ -3,3 +3,7 @@ aria2c --dir="$env:USERPROFILE\Downloads" --seed-time=0 "magnet:?xt=urn:btih:HAS
 
 # create symbolic link (run as admin)
 New-Item -ItemType SymbolicLink -Path "C:\Users\YourUsername\Desktop\MyLink.txt" -Target "C:\OriginalFolder\OriginalFile.txt"
+
+# add ublock and surfingkeys to edge extensions allowlist
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallAllowlist" -Name "99" -Value "cjpalhdlnbpafiamejdnhcphjbkeiagm" -PropertyType String -Force
+New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallAllowlist" -Name "98" -Value "gfbliohnnapiefjpjlpjnehglfpaknnc" -PropertyType String -Force
