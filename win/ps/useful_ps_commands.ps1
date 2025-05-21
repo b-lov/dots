ac15993d6a6175c2b413e9ee182a38fc14a2a434
b-lov <max@bilov.de>
2025-05-21 08:45:24 +0200

    useful command

--- win/ps/useful_ps_commands.ps1
+++
@@ -1,8 +0,0 @@
-# download torrent into Downloads with no seeding
-aria2c --dir="$env:USERPROFILE\Downloads" --seed-time=0 "magnet:?xt=urn:btih:HASH"
-
-# create symbolic link (run as admin)
-New-Item -ItemType SymbolicLink -Path "C:\Users\YourUsername\Desktop\MyLink.txt" -Target "C:\OriginalFolder\OriginalFile.txt"
-
-# add ublock and surfingkeys to edge extensions allowlist
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Edge\ExtensionInstallAllowlist" -Name "99" -Value "cjpalhdlnbpafiamejdnhcphjbkeiagm" -PropertyType String -Force
